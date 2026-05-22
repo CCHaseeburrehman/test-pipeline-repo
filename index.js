dd00925b07baea8a64579354a2677a2aa78250f8
CCHaseebburrehman <work.haseeburrehman@gmail.com>
2026-05-22 11:47:53 +0500

    push

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const crypto = require("crypto");
 const app = express();
 
 
-
+onst express = require("express");
+const crypto = require("crypto");
+const app = express();
 // Hardcoded secrets
 const apiKey = "12345-abcde";
 const dbPassword = "supersecretpassword123";
